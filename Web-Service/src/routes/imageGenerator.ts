d55db8cea140b2eb4f7ab8ac4e9c446624478665
Ryan Kang <ryan.dh.kang@gmail.com>
2018-10-08 23:07:27 -0700

    run python on node

--- Web-Service/src/routes/imageGenerator.ts
+++ Web-Service/src/routes/imageGenerator.ts
@@ -5,8 +5,26 @@ export class ImageGenerator {
     
     public routes(app): void {       
         app.route('/generateImage')
-        .get((req: Request, res: Response) => {            
-            res.status(200).send("hello world!");
+        .get((req: Request, res: Response) => {
+            const queryText = req.query.text;
+            const text = "'" + queryText+ "'";
+            console.log("QUERY")
+            console.log(text);
+
+            const { spawn } = require('child_process');
+            const pythonProcess = spawn('python3', ['/Users/rkang/Google Drive/School/MCSSE/Capstone/Text-to-Image-Synthesis-Web-Service/Core/ImageGenerator.py', text]);
+        
+            pythonProcess.stdout.on('data', (data) => {
+                console.log("DATA SUCCESS");
+                console.log(data.toString());
+                res.status(200).send("hello world!");
+            });
+            
+            pythonProcess.stderr.on('data', (data) => {
+                console.log("DATA FAILED");
+                console.log(data.toString());
+                res.status(200).send("hello world!");
+            });
         })               
     }
 }
