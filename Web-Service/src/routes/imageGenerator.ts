e3db7f1a0f7ffcf96fd6d4522320bd32f79fe45a
Ryan Kang <ryan.dh.kang@gmail.com>
2018-10-30 22:56:50 -0700

    add prettier lint

--- Web-Service/src/routes/imageGenerator.ts
+++ Web-Service/src/routes/imageGenerator.ts
@@ -1,46 +1,48 @@
+import { Request, Response } from 'express'
 
-import {Request, Response} from 'express';
+import * as path from 'path'
+import * as fs from 'fs'
 
-import * as path from 'path';
-import * as fs from 'fs';
-
-export class ImageGenerator { 
-    
-    public routes(app): void {       
-        app.route('/generateImage')
-        .get((req: Request, res: Response) => {
-            const queryText = req.query.text;
-            const sentence = "'" + queryText+ "'";
+export class ImageGenerator {
+    public routes(app): void {
+        app.route('/generateImage').get((req: Request, res: Response) => {
+            const queryText = req.query.text
+            const sentence = "'" + queryText + "'"
             console.log('QUERY String: ' + queryText)
 
-            const { spawn } = require('child_process');
-            const pythonFilePath = path.join(__dirname + '/../../../Core/ImageGenerator.py');
-            const pythonProcess = spawn('python3', [pythonFilePath, sentence]);
-        
+            const { spawn } = require('child_process')
+            const pythonFilePath = path.join(
+                __dirname + '/../../../Core/ImageGenerator.py'
+            )
+            const pythonProcess = spawn('python3', [pythonFilePath, sentence])
+
             let result = { responseCode: 0, resultDetail: 'String' }
-            pythonProcess.stdout.on('data', (data) => {
-                let returnedData =  data.toString().trim();
-                console.log('SUCCESS: ' + returnedData);
+            pythonProcess.stdout.on('data', data => {
+                let returnedData = data.toString().trim()
+                console.log('SUCCESS: ' + returnedData)
                 fs.stat(returnedData, function(err, data) {
                     if (err) {
-                        console.log(err);
-                        const errorString = 'Image could not be generated using the ' +
-                                            'given sentence: ' + queryText;
-                        res.status(500).send(errorString);
+                        console.log(err)
+                        const errorString =
+                            'Image could not be generated using the ' +
+                            'given sentence: ' +
+                            queryText
+                        res.status(500).send(errorString)
                     } else {
                         console.log('File exist!')
-                        res.sendFile(returnedData);
+                        res.sendFile(returnedData)
                     }
-                });
-            });
-            
-            pythonProcess.stderr.on('data', (data) => {
-                console.log('FAILED: ' + data.toString());
-                const errorString = 'Unexpected error occured while ' +
-                                    'generating the image for sentence: ' + 
-                                    queryText;
-                res.status(500).send(errorString);
-            });
-        })               
+                })
+            })
+
+            pythonProcess.stderr.on('data', data => {
+                console.log('FAILED: ' + data.toString())
+                const errorString =
+                    'Unexpected error occured while ' +
+                    'generating the image for sentence: ' +
+                    queryText
+                res.status(500).send(errorString)
+            })
+        })
     }
 }
